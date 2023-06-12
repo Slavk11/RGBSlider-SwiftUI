e709cae5aaf474ef480681376a9c550cbf5cc8d0
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-12 11:20:34 +0300

    Initial Commit

---
+++ RGBSlider-SwiftUI/RGBSlider_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSlider_SwiftUIApp.swift
+//  RGBSlider-SwiftUI
+//
+//  Created by Сазонов Станислав on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBSlider_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
